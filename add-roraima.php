e74257555b71732349d69879fc233ba6babbd7af
Adriano <adrianodasilva047@gmail.com>
2023-09-19 10:14:26 -0300

    tela para ver o retorno

--- add-roraima.php
+++ add-roraima.php
@@ -15,14 +15,17 @@ function am_insert_roraima() {
 	$tabela_estados = $wpdb->prefix .'state';
 	$tabela_cidades = $wpdb->prefix .'city';
 
+	$return = null;
+
 	//insere o estado
 	$sql_insere_estados = "INSERT INTO `". $tabela_estados ."` (`id`,`name`,`country_id`)"
 	."VALUES (NULL, 'Roraima', '31')";
-	$wpdb->query($sql_insere_estados);
+	$return .= 'Inseriu estado: '. $wpdb->query($sql_insere_estados) .'<br />';
 	
 	//pega o id do estado cadastrado
 	$estado_id = $wpdb->get_results("SELECT id FROM ". $tabela_estados. " WHERE name ='Roraima' LIMIT 1", OBJECT );
 	$id_estado = $estado_id[0]->id;
+	$return .= 'ID estado inserido: '. $id_estado .'<br />';
 
 	//insere as cidades
 	$sql_insere_cidades = "INSERT INTO `". $tabela_cidades ."` (`id`,`name`,`state_id`)"
@@ -30,9 +33,22 @@ function am_insert_roraima() {
 	."(NULL, 'Cantá', '". $id_estado ."'), (NULL, 'Caracaraí', '". $id_estado ."'), (NULL, 'Caroebe', '". $id_estado ."'), (NULL, 'Iracema', '". $id_estado ."'), "
 	."(NULL, 'Mucajaí', '". $id_estado ."'), (NULL, 'Normandia', '". $id_estado ."'), (NULL, 'Pacaraima', '". $id_estado ."'), (NULL, 'Rorainópolis', '". $id_estado ."'), "
 	."(NULL, 'São João da Baliza', '". $id_estado ."'), (NULL, 'São Luiz', '". $id_estado ."'), (NULL, 'Uiramutã', '". $id_estado ."')";
-	$wpdb->query($sql_insere_cidades);
+	$return .= 'Inseriru Cidades '. $wpdb->query($sql_insere_cidades);
 
+	return $return;
 	//executou já desativa o plugin
-	deactivate_plugins('add-roraima.php' );
+	//deactivate_plugins('add-roraima.php' );
 }
-add_action( 'activated_plugin', 'am_insert_roraima' );
+//add_action( 'activated_plugin', 'am_insert_roraima' );
+
+/*************************************************************
+ * menu
+ ************************************************************/
+function am_pipefy_menu() {
+	add_menu_page('AM', 'AM ADD Roraima','manage_options','am-add-roraima','am_add_contents','dashicons-block-default',8);
+}
+add_action( 'admin_menu', 'am_pipefy_menu' );
+
+function am_add_contents() {
+	include('am-add-page.php');
+}
---
+++ am-add-page.php
@@ -0,0 +1,6 @@
+<?php
+echo '<br /><br /><br /><br />'
+print_R(am_insert_roraima());
+
+echo '<br /><br /><h1>Você ja pode desativar e remover o plugin</h1>';
+?>
